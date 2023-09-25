3ad57809568d91aa62b865a5803785f292c5cb21
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2023-09-25 11:03:40 +0000

    geolocator setup

---
+++ lib/controller/global_controller.dart
@@ -0,0 +1,55 @@
+import 'package:geolocator/geolocator.dart';
+import 'package:get/get.dart';
+
+class GlobalController extends GetxController {
+  //create various variables
+  final RxBool _isLoading = true.obs;
+  final RxDouble _lattitude = 0.0.obs;
+  final RxDouble _longitude = 0.0.obs;
+
+  //instance for them to be called
+  RxBool checkLoading() => _isLoading;
+  RxDouble getLattitude() => _lattitude;
+  RxDouble getLongitude() => _longitude;
+
+  @override
+  void onInit() {
+    // TODO: implement onInit
+    if (_isLoading.isTrue) {
+      getLocation();
+    }
+    super.onInit();
+  }
+
+  getLocation() async {
+    bool isServiceEnabled;
+    LocationPermission locationPermission;
+
+    isServiceEnabled = await Geolocator.isLocationServiceEnabled();
+
+    //return if service is not enabled
+    if (!isServiceEnabled) {
+      return Future.error("Location not enabled");
+    }
+    //status of permission
+    locationPermission = await Geolocator.checkPermission();
+
+    if (locationPermission == LocationPermission.deniedForever) {
+      return Future.error("Location permisiion are denied forever");
+    } else if (locationPermission == LocationPermission.denied) {
+      //      request Permission
+      locationPermission = await Geolocator.requestPermission();
+      if (locationPermission == LocationPermission.denied) {
+        return Future.error("Permission is denied");
+      }
+    }
+    //    getting current position
+    return await Geolocator.getCurrentPosition(
+            desiredAccuracy: LocationAccuracy.best)
+        .then((value) {
+      //update latitude and longitude
+      _lattitude.value = value.latitude;
+      _longitude.value = value.longitude;
+    });
+  }
+}
