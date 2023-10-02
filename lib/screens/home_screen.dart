4ab716aa73d1c544bed5453820326dcb77bfc22b
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2023-10-02 10:28:53 +0000

    project complete

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:weatherapp_starter_project/controller/global_controller.dart';
+import 'package:weatherapp_starter_project/widgets/comfort_level.dart';
 import 'package:weatherapp_starter_project/widgets/current_weather_widget.dart';
 import 'package:weatherapp_starter_project/widgets/daily_data_forecast.dart';
 import 'package:weatherapp_starter_project/widgets/header_widget.dart';
 import 'package:weatherapp_starter_project/widgets/hourly_data_widget.dart';
+
+import '../utils/custom_colors.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -49,6 +52,17 @@ class _HomeScreenState extends State<HomeScreen> {
                       weatherDataDaily:
                           globalController.getData().getDailyWeather(),
                     ),
+                     Container(
+                      height: 1,
+                      color: CustomColors.dividerLine,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    ComfortLevel(
+                        weatherDataCurrent:
+                            globalController.getData().getCurrentWeather())
+                  
                   ],
                 ),
               ))
